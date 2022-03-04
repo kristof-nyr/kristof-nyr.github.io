932df014b6582e8995e090d35ddf61a8b97bf5e4
Kristóf Sándor Nyári <BFARTM@tr.pte.hu>
2022-03-04 11:31:57 +0100

    Created About page, small design changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import Landing from "./components/Landing";
 import Cursor from "./components/CircleCursor";
 import Motto from "./components/Motto";
+import About from "./components/About";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Motto />
       </section>
       <section id="about">
-
+        <About />
       </section>
       <section id="projects">
 
