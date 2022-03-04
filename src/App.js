c53d8d32d57f57c163e9b1a529bd934f95b39cef
Kristóf Sándor Nyári <BFARTM@tr.pte.hu>
2022-03-04 16:38:05 +0100

    Created about section (refactors due(

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from './components/Nav';
 import Landing from "./components/Landing";
-import Cursor from "./components/CircleCursor";
+// import Cursor from "./components/CircleCursor";
 import Motto from "./components/Motto";
 import About from "./components/About";
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,8 +20,25 @@ const About = () => {
                 software<span>engineer</span> & <span>design</span>enthusiast
             </h1>
         </motion.div>
-        <motion.div>
-            
+        <motion.div className="about-grid" initial="offscreen" whileInView="onscreen" variants={slidein} viewport={{once:true, amount:0.8}}>
+            <div className="about-pic">
+                {/* TODO: Maybe do a render (licensing problems?) */}
+                <img src="../../assets/holo_bg.jpg"></img>
+            </div>
+            {/* TODO: Refactor this */}
+            <div className="about-pic2">
+                <img src="../../assets/holo_bg2.jpg"></img>
+            </div>
+            <h2 className="about-me">
+                <span>Hello</span>there
+            </h2>
+            {/* TODO: Make a picture (3D type, without bg) */}
+            <p className="about-story">
+                Computer science student based in Hungary. Currently working as a Software Engineer. My fascination with visual stuff came at an early age as I was mesmerized by certain art styles. Thus began my career as a front-end dev, which later transformed into full-stack.
+            </p>
+            <p className="about-artsy">
+                I also believe self-expression and visual storytelling go hand in hand. Hence I aim to create visually pleasing works which invoke feelings in its viewers. 
+            </p>
         </motion.div>
         </>
     )
