48928084134c513b125fc1151c5f478dd7634492
kristof-nyr <kristof-nyr@DESKTOP-SFH5EMO>
2022-03-24 19:36:19 +0100

    Added Projects to the page - JS

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from "./components/Landing";
 // import Cursor from "./components/CircleCursor";
 import Motto from "./components/Motto";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
         <About />
       </section>
       <section id="projects">
-
+        <Projects />
       </section>
     </div>
   );
