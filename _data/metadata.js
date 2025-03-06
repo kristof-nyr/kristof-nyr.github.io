ec397a85217a316c080e74ed843ac74ae94bc045
Kristóf Nyári <nyari.kristof66@gmail.com>
2025-03-06 14:43:25 +0100

    feat: :bug: Tiny changes :)

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Kristof Nyari Portfolio",
+	url: "https://kristof-nyr.github.io/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Kristof Nyari",
+		email: "nyari.kristof66@gmail.com",
+		url: "https://kristof-nyr.github.io/about"
+	}
+}
